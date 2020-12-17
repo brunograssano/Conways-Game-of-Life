2ccc18ee5f7d863bc230498eb5d442ec17dda463
brunograssano <grassanobruno@gmail.com>
2020-12-17 11:38:45 -0300

    fix: ya no se para al mover el mouse si estaba andando

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,26 @@ mod board;
 use crate::board::Board;
 
 extern crate piston_window;
+use piston::input::{RenderEvent, UpdateEvent};
 use piston_window::*;
+use piston_window::keyboard::Key::{Return, P};
 
-fn draw(board: &mut Board, window: &mut PistonWindow, event: Event) {
-    let cells = board.get_cells();
-    window.draw_2d(&event, |context, graphics, _device| {
-        clear([1.0; 4], graphics);
-        for i in 0..100 {
-            for j in 0..100 {
-                if cells[i][j].is_alive() {
-                    let color: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-                    let position: [f64; 4] = [(6 * cells[i][j].get_x()) as f64, (6 * cells[i][j].get_y()) as f64, 5.0, 5.0];
-                    rectangle(color, position, context.transform, graphics);
+fn draw(board: &mut Board, window: &mut PistonWindow,event:Event) {
+        let background_color = [1.0; 4];
+        let cells = board.get_cells();
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear(background_color, graphics);
+            for i in 0..100 {
+                for j in 0..100 {
+                    if cells[i][j].is_alive() {
+                        let color: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+                        let position: [f64; 4] = [(6 * cells[i][j].get_x()) as f64, (6 * cells[i][j].get_y()) as f64, 5.0, 5.0];
+                        rectangle(color, position, context.transform, graphics);
+                    }
                 }
             }
-        }
-    });
+        });
+
 }
 
 fn main() {
@@ -30,33 +34,37 @@ fn main() {
     let mut window: PistonWindow = WindowSettings::new("Conway's Game of Life!", [600, 600])
                                                     .exit_on_esc(true).build().unwrap();
 
-    window.set_max_fps(30);
-
     let mut mouse_pos = (0.0,0.0);
     let mut start = false;
-    while !start {
-        if let Some(event) = window.next(){
-            if let Some(args) = event.button_args() {
-                if args.scancode == Some(28){
-                    start = true;
+    let event_settings = EventSettings::new();
+    let mut events = Events::new(event_settings);
+    while let Some(e) = events.next(&mut window) {
+        if let Some(_r) = e.render_args(){
+            draw(&mut board, &mut window,e);
+        }
+        else if let Some(_u)=e.update_args() {
+            if start {
+                board.update();
+            }
+        }
+        else if let Some(button) = e.press_args() {
+            match button{
+                Button::Mouse(button) => {
+                    let (x, y) = mouse_pos;
+                    board.toggle_cell((x / 6.0) as usize, (y / 6.0) as usize);
                 }
+                Button::Keyboard(button) => {
+                    if button == Key::P {
+                        start = false;
+                    } else if button == Key::Return {
+                        start = true;
+                    }
+                }
+                _ => {}
             }
-
-            if let Some(Button::Mouse(_button)) = event.press_args() {
-                let (x,y) = mouse_pos;
-                board.toggle_cell((x/6.0) as usize,(y/6.0) as usize);
-            }
-            if let Some(cursor) = event.mouse_cursor_args(){
-                mouse_pos = (cursor[0], cursor[1]);
-            }
-
-            draw(&mut board, &mut window, event);
+        }
+        else if let Some(cursor) = e.mouse_cursor_args() {
+            mouse_pos = (cursor[0], cursor[1]);
         }
     }
-
-    while let Some(event) = window.next() {
-        board.update();
-        draw(&mut board, &mut window, event);
-    }
-
 }
